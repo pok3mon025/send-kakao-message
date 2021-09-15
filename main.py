af7f367b52ece3947150af23395f67c8675e2e66
pok3mon025 <pok3mon025@gmail.com>
2021-09-15 22:03:45 +0900

    config 遺꾨━

--- main.py
+++ main.py
@@ -1,64 +1,54 @@
-import sys
 import xmltodict
-import json
-
-from twisted.python import log
-from urllib.request import urlopen, Request
-from urllib.parse import urlencode, quote_plus
 import requests
+import logger
+import config
+
+from datetime import datetime, timedelta
 
 
-def send_kakao_message(result):
-    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+def parse_xml_to_dict(xml_input):
+    try:
+        return xmltodict.parse(xml_input)
+    except xmltodict.expat.ExpatError as e:
+        logger.error("error: %s", e)
 
-    headers = {
-       "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        "Authorization": "Bearer {Kakao Token}"
-    }
 
-    data = {
-        "template_object": json.dumps(
-            {
-                "object_type": "text",
-                "text": result["resultMsg"],
-                "link": {
-                    "web_url": "https://developers.kakao.com",
-                    "mobile_web_url": "https://developers.kakao.com"
-                },
-                "button_title": "바로 확인"
-             }
-        )
-    }
+def send_kakao_message(message):
+    data = config.send_kakao_message_data
+    data['template_object']['text'] = message
 
-    with requests.post(url, headers=headers, data=data) as response:
+    try:
+        response = requests.post(config.SEND_KAKAO_MESSAGE_URL, headers=config.kakao_headers, data=data)
+
         if response.json().get("result_code") == 0:
             print("메시지를 성공적으로 보냈습니다.")
         else:
             print("메시지를 보내지 못했습니다. 오류메시지 : " + str(response.json()))
+    except requests.exceptions.RequestException as e:
+        logger.error("error: %s", e)
 
 
 if __name__ == "__main__":
-    log.startLogging(sys.stdout)
 
-    url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"
-    params = "?" + urlencode({
-        quote_plus("serviceKey"): "API Token",
-        quote_plus("pageNo"): "1",
-        quote_plus("numOfRows"): "10",
-        quote_plus("startCreateDt"): "20210914",
-        quote_plus("endCreateDt"): "20210914"
-    })
+    request_date = datetime.now() - timedelta(days=1)
 
-    request = Request(url + params)
-    request.get_method = lambda: "GET"
+    params = {
+        'serviceKey': config.COVID19_SERVICE_KEY,
+        'startCreateDt': request_date.strftime("%Y%m%d"),
+        'endCreateDt': request_date.strftime("%Y%m%d")
+    }
 
     try:
-        response = urlopen(request).read()
-        result = xmltodict.parse(response)["response"]["header"]
-        send_kakao_message(result)
-    except:
-        print("request error")
+        response = requests.get(config.COVID19_URL, params=params)
 
+        if response.ok:
+            result = parse_xml_to_dict(response.content)["response"]
+            if result['header']['resultCode'] == "00":
+                send_kakao_message("확진자 수 :" + result['body']['items']['item']['decideCnt'])
+            else:
+                send_kakao_message(result['header']['resultMsg'])
+    except requests.exceptions.RequestException as e:
+        logger.error("error: %s", e)
 
 
 
